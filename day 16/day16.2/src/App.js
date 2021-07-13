d171fa929f8a9b42ea5907278ee504753666a653
Imtesaal Ahmed <imtesaalahmed7@gmail.com>
2021-07-13 14:23:03 +0530

    JS file upddated

--- day 16/day16.2/src/App.js
+++ day 16/day16.2/src/App.js
@@ -2,21 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "card" >
+    return ( <div className = "card" >
         <
         div className = "image" >
         <
         img src = "https://i.pinimg.com/736x/dc/62/ba/dc62ba6https://memetemplateindia.com/wp-content/uploads/2020/05/aise-hi-sexy-lag-raha-tha-2.jpg433b6e744b095585c50883540.jpg"
         alt = "meme" >
-        <
-        /img> < /
-        div > <
-        h1 > Meme card < /h1> <
-        div className = "cardDescription" >
-        <
-        /div> < /
-        div >
+        </img> </div > <h1 > Meme card </h1> <div className = "cardDescription" >
+        </div> </div>
     );
 }
 
