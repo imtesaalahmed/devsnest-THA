1cb04e43380f513c817cf7dbe7bf4dd1a9c294a9
Imtesaal Ahmed <imtesaalahmed7@gmail.com>
2021-07-13 13:41:51 +0530

    Added THA 16 files

---
+++ day 16/day16.2/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "card" >
+        <
+        div className = "image" >
+        <
+        img src = "https://i.pinimg.com/736x/dc/62/ba/dc62ba6https://memetemplateindia.com/wp-content/uploads/2020/05/aise-hi-sexy-lag-raha-tha-2.jpg433b6e744b095585c50883540.jpg"
+        alt = "meme" >
+        <
+        /img> < /
+        div > <
+        h1 > Meme card < /h1> <
+        div className = "cardDescription" >
+        <
+        /div> < /
+        div >
+    );
+}
+
+export default App;
